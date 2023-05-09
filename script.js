012e973230059fd42b67858b0067f858bef2937a
tanughate05 <93458822+tanughate05@users.noreply.github.com>
2023-05-09 23:55:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your code here
+setInterval(() =>{
+	date= new Date();
+	htime= d.getHours();
+	mtime = d.getMinutes();
+	stime=d.getSeconds();
+	//formula
+	hrotation =30*htime + mtime/2;
+	mrotation =6*mtime;
+	srotation =6*stime;
+
+	hand hour-hand.style.transform='rotate(${hrotation}deg)';
+	hand min-hand.style.transform='rotate(${mrotation}deg)';
+	hand second-hand.style.transform='rotate(${srotation}deg)';
+},1000);
